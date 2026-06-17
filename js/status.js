5259c81baf70d961a00cb571fb5fb7995269d1ed
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 20:53:02 -0300

    Create status.js

---
+++ js/status.js
@@ -0,0 +1,33 @@
+const STATUS = {
+
+  atendido:{
+    icon:'<span style="color:#00ff9c">✔</span>',
+    class:"status-atendido"
+  },
+
+  concluido:{
+    icon:'<span style="color:#7CFC00">✔</span>',
+    class:"status-concluido"
+  },
+
+  andamento:{
+    icon:'<span style="color:#ffcc00">⏳</span>',
+    class:"status-andamento"
+  },
+
+  cancelado:{
+    icon:'<span style="color:#ff4d4d">✖</span>',
+    class:"status-cancelado"
+  },
+
+  manutenção:{
+    icon:'<span style="color:#ff0000">🔧</span>',
+    class:"status-manutencao"
+  },
+
+   viagem:{
+    icon:'<span style="color:#ff9800">✈️</span>',
+    class:"status-viagem"
+  }
+
+};

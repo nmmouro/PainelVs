4fdcd48b0b9cc355f3cbb42500226d4dacfbcdec
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 20:57:40 -0300

    Update veiculos.js

--- js/render/veiculos.js
+++ js/render/veiculos.js
@@ -1,1 +1,59 @@
+function renderVeiculos(rows, painelRows){
 
+  return `
+
+    <table>
+
+      <thead>
+        <tr>
+          <th>Placa</th>
+          <th>Status</th>
+          <th>Odômetro</th>
+          <th>Combustível</th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+        ${rows.map(row => {
+
+          const v =
+            (row.c || []).map(getCell);
+
+          return `
+
+            <tr>
+
+              <td>
+                <strong>${v[0] || ""}</strong>
+              </td>
+
+              <td>
+                ${getDisponibilidade(
+    v[0],
+    painelRows,
+    "veiculo"
+)}
+              </td>
+
+              <td>
+                ${v[2] || ""} km
+              </td>
+
+              <td>
+                ⛽ ${v[3] || ""}
+              </td>
+
+            </tr>
+
+          `;
+
+        }).join("")}
+
+      </tbody>
+
+    </table>
+
+  `;
+
+}

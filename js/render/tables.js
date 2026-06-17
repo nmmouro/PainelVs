d0033873866148af32cd57870dc623733bd02a30
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 20:55:32 -0300

    Update tables.js

--- js/render/tables.js
+++ js/render/tables.js
@@ -1,1 +1,64 @@
+function renderTable(headers, rows){
+
+  return `
+
+    <table>
+
+      <thead>
+
+        <tr>
+
+          ${headers.map(h => `
+            <th>${h}</th>
+          `).join("")}
+
+        </tr>
+
+      </thead>
+
+      <tbody>
+
+        ${rows.map(row => {
+
+          const values =
+            (row.c || []).map(getCell);
+
+          
+const status =
+  values.at(-1)?.toLowerCase() || "";
+
+const rowClass =
+  getStatusClass(status);
+
+const icon =
+  getStatusIcon(status);
+
+return `
+
+  <tr class="${rowClass}">
+
+    ${values.map((v,i)=>{
+
+      const isLast =
+        i === values.length - 1;
+
+      return `
+        <td class="${isLast ? 'status-icon' : ''}">
+
+          ${isLast ? icon : v}
+
+        </td>
+      `;
+    }).join("")}
+
+  </tr>
+`;
+
+    }).join("")}
+          
+      </tbody>
+
+    </table>
+  `;
+}
 

1beb7b7b96db0170814aeadc6e6ff58193c8fe1a
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 21:21:52 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
+let lastHash = "";
+let firstLoad = true;
 
+async function loadAll(){
+
+  const app =
+    document.getElementById("app");
+
+  try{
+
+    const results =
+      await Promise.all(
+
+        TABS.map(
+          t => fetchSheet(t.sheet)
+        )
+      );
+
+    const hash =
+      JSON.stringify(results);
+
+    if(
+      !firstLoad &&
+      hash !== lastHash
+    ){
+      playBeep();
+    }
+
+    firstLoad = false;
+    lastHash = hash;
+
+app.innerHTML = "";

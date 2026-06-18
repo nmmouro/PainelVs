8369b44d324dad2b829b4b7a336efb0a88ab067d
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 21:03:01 -0300

    Create fullscreen.js

---
+++ js/ui/fullscreen.js
@@ -0,0 +1,14 @@
+document.getElementById(
+  "toggle-fullscreen"
+).onclick = async () => {
+
+  if(!document.fullscreenElement){
+
+    await document.documentElement
+      .requestFullscreen();
+
+  }else{
+
+    await document.exitFullscreen();
+  }
+};

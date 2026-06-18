50ae205b1e72f085f2105836ab450d84f7f202b2
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-06-17 21:42:00 -0300

    Create tabs.js

---
+++ js/config/tabs.js
@@ -0,0 +1,32 @@
+export const TABS = [
+
+  {
+    id:"veiculos",
+    title:"VEÍCULOS",
+    sheet:"VEÍCULOS"
+  },
+
+  {
+    id:"motoristas",
+    title:"MOTORISTAS",
+    sheet:"MOTORISTAS"
+  },
+
+  {
+    id:"painel",
+    title:"PAINEL",
+    sheet:"LANÇAMENTOS"
+  },
+
+  {
+    id:"agenda",
+    title:"AGENDA",
+    sheet:"AGENDA DO DIA"
+  }
+
+  id:"social",
+    title:"AGENDA SERVIÇO SOCIAL",
+    sheet:"AGENDA SERVIÇO SOCIAL",
+  }
+
+];
